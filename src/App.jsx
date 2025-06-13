9a319e6d61f6e009f853f0fbb3eba97d139f63f2
AMOB-Kahuna <balloabdulmujeeb42@gmail.com>
2025-06-13 09:39:23 +0100

    I create the profile info section

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,24 @@ function App() {
       </section>
 
       <section className="profile-information">
+        <div>
+          <h1>Abdulmujeeb Ballo</h1>
+          <p>frontend developer</p>
 
+          <div className="mail">
+            <a href="mailto:balloabdulmujeeb42@gmail.com" className='btn mail-btn'><i className="fa fa-envelope" aria-hidden="true"></i> Email</a>
+
+            <a href="https://www.linkedin.com" className='btn link-btn'><i className="fa fa-linkedin-square" aria-hidden="true"></i> LinkedIn</a>
+          </div>
+
+          <div className="about">
+            <h2>About</h2>
+            <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+
+            <h2>Interests</h2>
+            <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+          </div>
+        </div>
       </section>
 
       <section className="social-handles">
